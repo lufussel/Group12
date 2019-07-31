a1dd4c7d3b27d7c7deb58caa9efc2a4edf6ac7a5
Luke Fussell <lufussel@microsoft.com>
2019-07-31 16:40:19 +0100

    Reformat

--- scripts/initializeVm/installWebRole.ps1
+++ scripts/initializeVm/installWebRole.ps1
@@ -1,15 +1,1 @@
-<#
-
-Script to install default IIS and the management tools
-
-KY V0.1 15/03/2018
-
-#>
-
-## If computer is like mweb then install the role
-Write-host "Installing web default web role and management tools"
-add-windowsfeature web-server -includemanagementtools
-Write-Host "Reboot your server please"
-
-##add-windowsfeature net-framework-core
-##[Environment]::OSVersion.VersionString
+Add-WindowsFeature web-server -IncludeManagementTools
